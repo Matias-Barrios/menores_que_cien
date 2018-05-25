ad26677a335d37e67ccf67542146076666f33e85
Matias-Barrios <soymatiasbarrios@gmail.com>
2018-05-25 11:26:54 -0500

    Add project files.

---
+++ menores_que_cien/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.calcular = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 93)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(238, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cantidad de numeros menores que cien :"
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResultado.ForeColor = System.Drawing.Color.OrangeRed
+        Me.lblResultado.Location = New System.Drawing.Point(264, 93)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(14, 13)
+        Me.lblResultado.TabIndex = 1
+        Me.lblResultado.Text = "0"
+        '
+        'calcular
+        '
+        Me.calcular.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.calcular.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.calcular.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.calcular.Location = New System.Drawing.Point(15, 27)
+        Me.calcular.Name = "calcular"
+        Me.calcular.Size = New System.Drawing.Size(280, 45)
+        Me.calcular.TabIndex = 2
+        Me.calcular.Text = "Ingresar numeros"
+        Me.calcular.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 131)
+        Me.Controls.Add(Me.calcular)
+        Me.Controls.Add(Me.lblResultado)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Menores que cien?"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblResultado As Label
+    Friend WithEvents calcular As Button
+End Class
---
+++ menores_que_cien/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Dim numeros(9) As Integer
+
+    Private Sub calculo(sender As Object, e As EventArgs) Handles calcular.Click
+        For Each numero In numeros
+            numero = Val(InputBox(" Ingrese un numero cualquera... "))
+            If numero < 100 Then
+                lblResultado.Text = Val(lblResultado.Text) + 1
+            End If
+        Next
+    End Sub
+
+End Class
